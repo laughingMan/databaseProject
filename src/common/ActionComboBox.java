ba367d7a6bade014ebca77720cd5db61dabc2a6b
laughingMan <isaac.hatton@gmail.com>
2013-12-02 13:52:17 -0600

    added return movie

--- src/common/ActionComboBox.java
+++ src/common/ActionComboBox.java
@@ -4,8 +4,8 @@ import javax.swing.JComboBox;
 
 public class ActionComboBox extends JComboBox<String> {
 	private static final long serialVersionUID = 1L;
-	private final static String[] actionDropDownOptions = { "Rent Movie", "Create Customer", "Edit Customer", "Remove Customer", "Add Movie", "Edit Movie",
-			"Remove Movie", "Customer Statistics", "Movie Statistics" };
+	private final static String[] actionDropDownOptions = { "Rent Movie", "Return Movie", "Create Customer", "Edit Customer", "Remove Customer", "Add Movie",
+			"Edit Movie", "Remove Movie", "Customer Statistics", "Movie Statistics" };
 
 	public ActionComboBox() {
 		super(actionDropDownOptions);
